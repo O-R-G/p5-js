1f5b4b6398a10fe899d71874d80ff32b4d56c973
david reinfurt <reinfurt@o-r-g.com>
2026-06-08 14:09:01 -0400

    ibid

--- js/infinity.js
+++ js/infinity.js
@@ -45,14 +45,15 @@ function setup() {
     path = new svgPathProperties.svgPathProperties(pathData);
     pathLength = path.getTotalLength();
     red = color(255,0,0);
-    green = color(0,255,0);
+    // green = color(0,255,0);
+    green = color(0,0,230);
 }
 
 function draw() {
     // background(50,0,0);
-    background(0,30,0);
+    // background(0,30,0);
     // background(0,30,30);
-    // background(0,0,0);
+    background(0,0,0);
     translate(70,70);
     scale(1);
     if (running) {
